692226b60b6cfe1bc0ab522483f88ae71573c73c
Krzysztof Kuczewski <k.kuczewski10@gmail.com>
2026-02-27 17:13:13 +0100

    Made the setup.sh file more descriptive

--- setup.sh
+++ setup.sh
@@ -3,7 +3,13 @@
 set -e
 
 echo '{"lingos": []}' > db.json
-touch .env
+if [ -e ".env" ]; then
+	echo -e "\n.env already exists\n"
+else
+	echo -e "\nCreating an example .env file...\n"
+	cat .env.example | tee .env
+fi
 
+echo -e "Creating python venv and installing dependencies...\n"
 python -m venv venv
 ./venv/bin/python -m pip install -r requirements.txt
